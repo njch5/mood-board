89a1fac2b57dca08542a5c9a75a095fe3d90b2ef
Nicky Choi <nickyjinchoi@gmail.com>
2021-03-22 22:01:14 -0700

    Initial commit

---
+++ src/features/counter/Board.js
@@ -0,0 +1,6 @@
+import SpotifyWebApi from "spotify-web-api-js";
+
+var spotifyApi = new SpotifyWebApi();
+
+spotifyApi.setAccessToken();
+// https://stackabuse.com/encoding-and-decoding-base64-strings-in-node-js/
